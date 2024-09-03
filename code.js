02a803e598b57778586d3b07de32c7ebc95161b2
MKdivine <jensboerger23@gmail.com>
2024-09-03 04:09:07 +0200

    yo

--- code.js
+++ code.js
@@ -3,46 +3,40 @@ const humanScore = 0;
 const computerScore = 0;
 
 function getComputerChoice() {
-  console.log("PC CHOICE: ")
+  console.log("PC CHOICE: ");
   console.log(choice[Math.floor(Math.random() * 3)]);
 }
 
 getComputerChoice();
 
 function getHumanChoice() {
- // return the choice alert
-  let userInput = prompt("Enter your choice bro");
+  // return the choice alert
+  let userInput = prompt("Enter your choice").toLowerCase();
   if (choice.includes(userInput)) {
     //return userInput;
     console.log(`You chose ${userInput}`);
+    return userInput;
+  }
+}
+getHumanChoice();
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log("You win!");
+    humanScore++;
+  } else {
+    console.log("You lose!");
+    computerScore++;
   }
 }
 
-getHumanChoice();
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
-// function playRound(humanChoice, computerChoice) {
-//   if (humanChoice === computerChoice) {
-//     console.log("It's a tie");
-//   }
-//   else if (humanChoice === "Rock" && computerChoice === "Scissors") {
-//     console.log("You win this round");
-//     humanScore++;
-//   }
-//   else if (humanChoice === "Paper" && computerChoice === "Rock") {
-//     console.log("You win");
-//     humanScore++;
-//   }
-//   else if (humanChoice === "Scissors" && computerChoice === "Paper") {
-//     console.log("You win");
-//     humanScore++;
-//   }
-//   else {
-//     console.log("You lose");
-//     computerScore++;
-//   }
-// }
-
-// const humanSelection = getHumanChoice();
-// const computerSelection = getComputerChoice();
-
-// playRound(humanSelection, computerSelection);
+playRound(humanSelection, computerSelection);

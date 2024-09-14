0bc52c2f8759cc8df2abb1b96c11723ea4370067
MKdivine <jensboerger23@gmail.com>
2024-09-14 18:34:57 +0200

    added new stuff

--- code.js
+++ code.js
@@ -2,56 +2,54 @@ const choice = ["rock", "paper", "scissors"];
 
 // Button functionality and start the game
 
-function playGame(humanChoice, computerChoice) {
+function playGame() {
   let humanScore = 0;
   let computerScore = 0;
+  
+  while (humanScore < 5 && computerScore < 5) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
 
-  if (humanChoice === computerChoice) {
-    console.log("It's a tie!");
-  } else if (
-    (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "paper" && computerChoice === "rock") ||
-    (humanChoice === "scissors" && computerChoice === "paper")
-  ) {
-    console.log("You win!");
-    humanScore++;
-  } else {
-    console.log("You lose!");
-    computerScore++;
-  }
-
-    while (humanScore < 5 && computerScore < 5) {
-      playGame();
-    }
-    if (humanScore > computerScore) {
-      console.log("You won the game!");
+    if (humanSelection === computerSelection) {
+      console.log("It's a tie!");
+    } else if (
+      (humanSelection === "rock" && computerSelection === "scissors") ||
+      (humanSelection === "paper" && computerSelection === "rock") ||
+      (humanSelection === "scissors" && computerSelection === "paper")
+    ) {
+      console.log("You win!");
+      humanScore++;
     } else {
-      console.log("You lost the game!");
+      console.log("You lose!");
+      computerScore++;
     }
+
+    console.log(`Your score: ${humanScore}`);
+    console.log(`Computer score: ${computerScore}`);
   }
 
-
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
-
-  playGame(humanSelection, computerSelection);
-
-
-  // Game PC Choice
-  function getComputerChoice() {
-    const weed = choice[Math.floor(Math.random() * 3)];
-    console.log(`The computer chose ${weed}`);
-    return weed;
+  if (humanScore === 5) {
+    console.log("You are the winner!");
+  } else {
+    console.log("The computer is the winner!");
   }
+}
 
-  // Game Human Choice
-  function getHumanChoice() {
-    // return the choice alert
-    let userInput = prompt("Enter your choice").toLowerCase();
-    if (choice.includes(userInput)) {
-      //return userInput;
-      console.log(`You chose ${userInput}`);
-      return userInput;
-    }
+// Game PC Choice
+function getComputerChoice() {
+  const weed = choice[Math.floor(Math.random() * 3)];
+  console.log(`The computer chose ${weed}`);
+  return weed;
+}
+
+// Game Human Choice
+function getHumanChoice() {
+  let userInput = prompt("Enter your choice").toLowerCase();
+  if (choice.includes(userInput)) {
+    console.log(`You chose ${userInput}`);
+    return userInput;
   }
+}
 
+// Start the game
+playGame();
